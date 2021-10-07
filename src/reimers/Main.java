399442a078540f88eb06f079c50c00b187818707
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2021-10-06 20:29:11 -0600

    Got rid of accidental double parentheses

--- src/reimers/Main.java
+++ src/reimers/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name Justin.");
-        System.out.println(("Hello Justin, my name is Kaylee."));
+        System.out.println("Hello Justin, my name is Kaylee.");
     }
 }
